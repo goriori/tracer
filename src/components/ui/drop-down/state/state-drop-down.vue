2a33f185bc9e8b0145758b147705295617ba2e1f
goriori <vladislav.borisov2010@gmail.com>
2024-06-25 15:58:01 +0400

    feat: - add ui button elements and icons - add action state-editor-action.vue for change state editor - add emits in Header component and Editor page.

---
+++ src/components/ui/drop-down/state/state-drop-down.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import DropDown from "@/components/ui/drop-down/drop-down.vue";
+import {markRaw} from "vue";
+import SvgButton from "@/components/ui/button/svg/svg-button.vue";
+import MapButton from "@/components/ui/button/map/map-button.vue";
+
+const emits = defineEmits(['change-state-editor'])
+const list = [markRaw(SvgButton), markRaw(MapButton)]
+const stateEvents = {
+  svg: () => emits('change-state-editor', 'svg_editor'),
+  map: () => emits('change-state-editor', 'tracer')
+}
+const onChange = (element) => {
+  const elementName = element.__name
+  const nameEvent = elementName.split('-')[0]
+  return stateEvents[nameEvent]()
+}
+</script>
+
+<template>
+  <DropDown :list="list" @change="onChange"/>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/store/applicationStore.js
+++ src/store/applicationStore.js
@@ -4,16 +4,22 @@ import {Brush} from "@/entities/brush/index.js";
 import {MapSVG} from "@/entities/map/index.js";
 
 export const useApplicationStore = defineStore('application', () => {
+    const STATES = ['tracer', 'svg_editor']
     const svgMap = ref(null)
+    const editorState = ref(STATES[0])
     const downloadSvg = (svg) => {
         svgMap.value = new MapSVG('tracer', svg)
     }
     const getMapSvg = () => {
         return svgMap.value
     }
+    const getStateEditor = () => editorState.value
+    const changeStateEditor = (state = STATES[0]) => editorState.value = state
     return {
         svgMap,
         downloadSvg,
-        getMapSvg
+        getMapSvg,
+        getStateEditor,
+        changeStateEditor
     }
 })
